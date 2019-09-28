3c1c46c8a3c9ede40494a75ab3dbce0ba01b1f34
kashwp7 <33324119+kashwp7@users.noreply.github.com>
2019-09-28 20:22:39 +0800

    New test code

---
+++ Testing Code/SerialDisplay/SerialDisplay.ino
@@ -0,0 +1,44 @@
+/*
+University of Western Australia
+ELEC5552 - Semester 2 2019 - Project 9
+
+ The circuit:
+ * LCD RS pin to digital pin 8
+ * LCD Enable pin to digital pin 9
+ * LCD D4 pin to digital pin 4
+ * LCD D5 pin to digital pin 5
+ * LCD D6 pin to digital pin 6
+ * LCD D7 pin to digital pin 7
+ * LCD R/W pin to ground
+
+*/
+
+//Libraries
+#include <LiquidCrystal.h>
+
+//Initialisation
+//--LCD
+const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // initialize the serial communications:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // when characters arrive over the serial port...
+  if (Serial.available()) {
+    // wait a bit for the entire message to arrive
+    delay(100);
+    // clear the screen
+    lcd.clear();
+    // read all the available characters
+    while (Serial.available() > 0) {
+      // display each character to the LCD
+      lcd.write(Serial.read());
+    }
+  }
+}
